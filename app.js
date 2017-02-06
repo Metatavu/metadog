a046644f700f098549ca73955909f7ebcb294fad
Heikki Kurhinen <heikki.kurhinen@metatavu.fi>
2017-02-06 20:17:51 +0200

    Renamed project

--- app.js
+++ app.js
@@ -6,12 +6,83 @@
   const config = require(__dirname + '/config.json');
 
   const fcm = new FCM(config.serverKey);
-  const http = require('http');
   const express = require('express');
   const bodyParser = require('body-parser');
   const util = require('util');
 
-  var app = express();
+  const app = express();
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http);
+
+  const notificationTimers = {};
+
+  function getServer(text) {
+    var lines = text.split('\n');
+    var server = null;
+    for (let i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      if (line.indexOf('Host:') > -1) {
+        server = line.split(':')[1].trim();
+        break;
+      }
+    }
+    return server;
+  }
+
+  function handleCritical(data) {
+    var server = getServer(data['body-plain']);
+    if (server) {
+      io.emit('server:critical', {
+        server: server
+      });
+      var subject = data.subject;
+      if (!notificationTimers[server]) {
+        notificationTimers[server] = setTimeout(() => {
+          var body = data['body-plain'];
+          var message = {
+            to: '/topics/onduty',
+            data: {
+              serverId: server
+            },
+            notification: {
+              title: subject,
+              body: body,
+              sound: 'missile'
+            }
+          };
+
+          fcm.send(message)
+            .then(function (response) {
+              console.log(util.format('Successfully sent notification about server: %s', server));
+            })
+            .catch(function (err) {
+              console.error(util.format('Error when sending notification: %s', err));
+            });
+        }, 300000);
+      }
+    }
+  }
+
+  function handleWarning(data) {
+    var server = getServer(data['body-plain']);
+    if (server) {
+      io.emit('server:warning', {
+        server: server
+      });
+    }
+  }
+
+  function handleRecovery(data) {
+    var server = getServer(data['body-plain']);
+    if (server) {
+      io.emit('server:recovery', {
+        server: server
+      });
+      if (notificationTimers[server]) {
+        clearTimeout(notificationTimers[server]);
+      }
+    }
+  }
 
   app.set('port', config.port);
   app.use(bodyParser.json());
@@ -19,47 +90,25 @@
     extended: true
   }));
 
-  app.post('/alert', (req, res) => {
-    var subject = req.body.subject;
-    if (subject.indexOf('CRITICAL') > -1) {
-      var server = null;
-      for (let i = 0; i < config.servers.length; i++) {
-        if (subject.indexOf(config.servers[i]) > -1) {
-          server = config.servers[i];
-          break;
-        }
-      }
-      if (server) {
-        var body = req.body['body-plain'];
-        var message = {
-          to: '/topics/onduty',
-          data: {
-            serverId: server
-          },
-          notification: {
-            title: subject,
-            body: body,
-            sound: 'missile'
-          }
-        };
-
-        fcm.send(message)
-          .then(function (response) {
-            console.log(util.format('Successfully sent notification about server: %s', server));
-            res.send('ok');
-          })
-          .catch(function (err) {
-            console.error(util.format('Error when sending notification: %s', err));
-            res.send('ok');
-          });
-
-      } else {
-        res.send('ok');
-      }
-    }
+  io.on('connection', (socket) => {
+    socket.on('')
   });
 
-  http.createServer(app).listen(app.get('port'), function () {
+  app.post('/alert', (req, res) => {
+    var subject = req.body.subject;
+
+    if (subject.indexOf('CRITICAL') > -1) {
+      handleCritical(req.body);
+    } else if (subject.indexOf('WARNING') > -1) {
+      handleWarning(req.body);
+    } else if (subject.indexOf('RECOVERY') > -1) {
+      handleRecovery(req.body);
+    }
+
+    res.send('ok');
+  });
+
+  http.listen(config.port, () => {
     console.log(util.format('Listening on port: %s', app.get('port')));
   });
 
